72e154047096d43a0c807f3a9a6e8301fa989980
James Randall <james@manmachineltd.com>
2024-11-12 10:23:43 +1100

    Added schema controller and basic routing

---
+++ app/Http/Controllers/SchemaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SchemaController extends Controller
+{
+    //
+    public function index()
+    {
+        return response()->json(['Thanks for coming!']);
+    }
+}
